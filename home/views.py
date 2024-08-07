dc87f4602d234e99eb1b3988cf6db2d1a5139763
LucazMedeiroz <lucasmedeiros@ua.pt>
2024-08-07 15:13:49 +0100

    teste

--- home/views.py
+++ home/views.py
@@ -27,7 +27,7 @@ def login_view(request):
         
         if user is not None:
             login(request, user)
-            return redirect('validadorGeral')
+            return redirect('home')
         else:
             return redirect('login')
     return render(request, 'home/login.html')
